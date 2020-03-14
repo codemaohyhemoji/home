12355af3b84dc500fc73690059e9e3fd2472e3aa
codemaohyhemoji <62126681+codemaohyhemoji@users.noreply.github.com>
2020-03-14 13:06:34 +0800

    Update lockkey.js

--- logjs/lockkey.js
+++ logjs/lockkey.js
@@ -14,3 +14,16 @@ document.onkeydown = function(){
       }
   
   }
+function checkCookie()
+{
+  var username=getCookie("username");
+  if (username!="")
+  {
+    document.getElementById("login").innerHTML = "编程猫后援会表情部，您好，"+username;
+  }
+  else 
+  {
+    window.location.href 1 window.location.href="https://codemaohyhemoji.github.io/home/work.html"
+    }
+  }
+}
