5fdb72519e9b8336c875173578210ffab1e06ea9
codemaohyhemoji <62126681+codemaohyhemoji@users.noreply.github.com>
2020-03-14 16:45:21 +0800

    Update cookie.js

--- logjs/cookie.js
+++ logjs/cookie.js
@@ -8,3 +8,14 @@ var username=getCookie("username");
     window.location.href="https://codemaohyhemoji.github.io/home/work.html"
     }
 
+function getCookie(cname)
+{
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) 
+  {
+    var c = ca[i].trim();
+    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
