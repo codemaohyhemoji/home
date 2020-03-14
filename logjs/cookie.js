15a44ac917240e9bca2771f95b353d8753cd8f44
codemaohyhemoji <62126681+codemaohyhemoji@users.noreply.github.com>
2020-03-14 12:31:45 +0800

    Add files via upload

---
+++ logjs/cookie.js
@@ -0,0 +1,13 @@
+function checkCookie()
+{
+  var username=getCookie("username");
+  if (username!="")
+  {
+    document.getElementById("login").innerHTML = "编程猫后援会表情部，您好，"+username;
+  }
+  else 
+  {
+    window.location.href 1 window.location.href="https://codemaohyhemoji.github.io/home/work.html"
+    }
+  }
+}
